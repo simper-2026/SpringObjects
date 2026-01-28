ed021f97e2eeebcb4f8bff25021fbde2b83df0ac
Dallan Simper <dallan.simper@gmail.com>
2026-01-28 09:15:14 -0700

    pointers

--- Program.cpp
+++ Program.cpp
@@ -5,55 +5,66 @@
 
 using namespace std;
 
-int add(int a, int b);
 
 void MemoryLeak() {
-	long long limit = 10000;
+	long long limit = 1000000;
+	int* ptr = nullptr;
 	for (int i = 0; i < limit; i++)
 	{
-		new int[limit];
+		ptr = new int[limit];
+		delete ptr;
 	}
 }
 
 void add (int a, int b, int &s){
+	cout << "s: " << &s << endl;
+	cout << "a: " << &a << endl;
+	cout << "b: " << &b << endl;
+	
 	s = a + b;
 }
 
 int main() {
+	MemoryLeak();
+
+	int a = 3;
+	int* p = nullptr;
+	p = new int;
+
+
+	switch (a)
+	{
+	case 3:
+		cout << "Three";
+		//break;
+	case 2:
+		cout << "Two";
+		break;
+	default:
+		break;
+	}
+
+	int* ptr = nullptr;
+	ptr = new int[10];
+
+	*ptr = 12;
+	*(ptr + 1) = 13;
+	ptr[2] = 48;
+
+	ptr++;
 
 	//MemoryLeak();
+	
+	int num, number, sum;
+	num = 1;
+	number = 2;
+	sum = 27;
 
-	cout << sizeof(bool) << endl;
-	cout << sizeof(char) << endl;
-	cout << sizeof(int) << endl;
-	cout << sizeof(short) << endl;
-	cout << sizeof(double) << endl;
-	cout << sizeof(float) << endl;
-
-	vector<int> list;// = { 1,2,3,4,6,7,7,8 };
-
-
-	for (auto element : list) {
-		cout << element << ", ";
-	}
-	cout << endl;
-	for (int i = 0; i < list.size(); i++)
-	{
-		cout << list[i] << ", ";
-	}
-	cout << endl;
-
-	int j = 0;
-	while (j < list.size()) {
-		cout << list[j] << ", ";
-
-		j++;
-	}
+	add(num, number, sum);
+	cout << sum <<  endl;
+	cout << "sum: " << &sum << endl;
+	cout << "num: " << &num << endl;
+	cout << "number: " << &number << endl;
 
 }
 
-
-
-int add(int a, int b) {
-	return a + b;
-}
