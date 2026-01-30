2e0d0f065288e669dca58598440f9978e5776db1
Dallan Simper <dallan.simper@gmail.com>
2026-01-30 09:06:40 -0700

    info hiding

--- Program.cpp
+++ Program.cpp
@@ -6,65 +6,41 @@
 using namespace std;
 
 
-void MemoryLeak() {
-	long long limit = 1000000;
-	int* ptr = nullptr;
-	for (int i = 0; i < limit; i++)
-	{
-		ptr = new int[limit];
-		delete ptr;
-	}
-}
+class Rectangle {
+private:
+	double length;
+	double width;
+	double area;
 
-void add (int a, int b, int &s){
-	cout << "s: " << &s << endl;
-	cout << "a: " << &a << endl;
-	cout << "b: " << &b << endl;
-	
-	s = a + b;
-}
+	void UpdateArea() {
+		area = length * width;
+	}
+public:
+
+	Rectangle(int l, int w) {
+		length = l;
+		width = w;
+		UpdateArea();
+	}
+
+	int GetArea() {
+		return area;
+	}
+	void SetLength(int l) {
+		length = l;
+		UpdateArea();
+	}
+};
+
+
+//cin.ignore()
 
 int main() {
-	MemoryLeak();
+	Rectangle r1(3,4);
 
-	int a = 3;
-	int* p = nullptr;
-	p = new int;
-
-
-	switch (a)
-	{
-	case 3:
-		cout << "Three";
-		//break;
-	case 2:
-		cout << "Two";
-		break;
-	default:
-		break;
-	}
-
-	int* ptr = nullptr;
-	ptr = new int[10];
-
-	*ptr = 12;
-	*(ptr + 1) = 13;
-	ptr[2] = 48;
-
-	ptr++;
-
-	//MemoryLeak();
-	
-	int num, number, sum;
-	num = 1;
-	number = 2;
-	sum = 27;
-
-	add(num, number, sum);
-	cout << sum <<  endl;
-	cout << "sum: " << &sum << endl;
-	cout << "num: " << &num << endl;
-	cout << "number: " << &number << endl;
+	cout << "Area: " << r1.GetArea() << endl;
+	r1.SetLength(2);
+	cout << "Area: " << r1.GetArea() << endl;
 
 }
 
