708938b5c109ffdaa124ebd84fcd94be634d3345
Dallan Simper <dallan.simper@gmail.com>
2026-03-25 08:52:08 -0600

    threads

--- Program.cpp
+++ Program.cpp
@@ -18,19 +18,26 @@ void Move(Piece* p) {
 }
 
 
-//cin.ignore()
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+void printLoop(int id, int delayMs) {
+    for (int i = 1; i <= 20; i++) {
+        //std::cout << "Thread " << id << " - loop " << i << " (before sleep) - " << (i - 1)*delayMs << " ms\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(delayMs));
+        std::cout << "Thread " << id << " - loop " << i << " - " << i * delayMs << " ms\n";
+    }
+}
 
 int main() {
+    std::thread t1(printLoop, 1, 300);  // sleeps 300ms each iteration
+    std::thread t2(printLoop, 2, 500);  // sleeps 500ms each iteration
 
-	/*Board b;
+    t1.join();
+    t2.join();
 
-	cout << b.ToString();*/
-
-
-	vector<int> a;
-
-	cout << a.max_size();
-
-
+    std::cout << "Both threads finished\n";
+    return 0;
 }
 
